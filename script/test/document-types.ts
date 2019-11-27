d800740209ba6b9d4d98076ba7499183dd9a89b1
yuta0801 <yuta20030801@icloud.com>
2019-11-28 04:46:39 +0900

    Add collection repo to content

--- script/test/document-types.ts
+++ script/test/document-types.ts
@@ -3,6 +3,7 @@ import { Document } from '../lib/document-types'
 import * as discordjs_oldest from '../../source/discord.js/9.0.2.json'
 import * as discordjs_stable from '../../source/discord.js/stable.json'
 import * as discordjs_master from '../../source/discord.js/master.json'
+import * as collection_master from '../../source/collection/master.json'
 import * as commando_oldest from '../../source/commando/v0.5.0.json'
 import * as commando_master from '../../source/commando/master.json'
 import * as rpc_master from '../../source/rpc/master.json'
@@ -11,6 +12,7 @@ const _docs: Document[] = [
   discordjs_oldest, // discord.js/9.0.2.json
   discordjs_stable, // discord.js/stable.json
   discordjs_master, // discord.js/master.json
+  collection_master, // collection/master.json
   commando_oldest, // Commando/v0.5.0.json
   commando_master, // Commando/master.json
   rpc_master, // RPC/master.json
