fd113a3945d93a653d6967fec610ad202c805c4d
yuta0801 <yuta20030801@icloud.com>
2019-11-29 20:51:16 +0900

    Fix import path case

--- script/test/document-types.ts
+++ script/test/document-types.ts
@@ -4,9 +4,9 @@ import * as discordjs_oldest from '../../source/discord.js/9.0.2.json'
 import * as discordjs_stable from '../../source/discord.js/stable.json'
 import * as discordjs_master from '../../source/discord.js/master.json'
 import * as collection_master from '../../source/collection/master.json'
-import * as commando_oldest from '../../source/commando/v0.5.0.json'
-import * as commando_master from '../../source/commando/master.json'
-import * as rpc_master from '../../source/rpc/master.json'
+import * as commando_oldest from '../../source/Commando/v0.5.0.json'
+import * as commando_master from '../../source/Commando/master.json'
+import * as rpc_master from '../../source/RPC/master.json'
 
 const _docs: Document[] = [
   discordjs_oldest, // discord.js/9.0.2.json
